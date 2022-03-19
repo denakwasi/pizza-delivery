9aab3d6c47d95093864f9dfd411141d9c6e075ff
denakwasi <denakwasi@gmail.com>
2022-03-19 22:29:18 +0000

    Cors origins allow

--- pizza/settings.py
+++ pizza/settings.py
@@ -163,3 +163,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
