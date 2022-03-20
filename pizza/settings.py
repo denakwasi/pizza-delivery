237f66d4c5aeb8cf93abdf3ca5f997d75a29bddc
denakwasi <denakwasi@gmail.com>
2022-03-20 22:07:18 +0000

    Cors properly fixed

--- pizza/settings.py
+++ pizza/settings.py
@@ -167,5 +167,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http//localhost:3000"
+    "http://localhost:3000"
 ]
