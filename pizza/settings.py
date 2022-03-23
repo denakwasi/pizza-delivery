e01a6b89d9360a8520ef1bfe6231c6bf7bd85c7b
denakwasi <denakwasi@gmail.com>
2022-03-23 19:09:51 +0000

    second fix

--- pizza/settings.py
+++ pizza/settings.py
@@ -162,8 +162,8 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
